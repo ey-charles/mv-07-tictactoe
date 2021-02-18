5b39b7ede0d4b39739439f1b8150e50ab46bfc25
Camilo Patino <jcamilop96@gmail.com>
2021-02-18 14:43:17 -0500

    Add game UI for win, invalid input and draw conditions

--- bin/main.rb
+++ bin/main.rb
@@ -24,17 +24,79 @@ p '-----------------'
 puts "\n\n"
 
 # game = Game.new
+
+# game.show_board
 p '   A B C'
 p ' ________'
 p '1| - - - |'
 p '2| - - - |'
 p '3| - - - |'
 p ' ________'
+puts "\n\n"
 
-#
-# game.show_board
 # game.add_player(player_1_name, player_1_token)
 # game.add_player(player_2_name, player_2_token)
+
+# until game.has_winner? || game.turns > 9 do new turn:
+p "Carlos\' turn:"
 # game.new_turn("Carlos", "A2")
 # game.show_board
+p '   A B C'
+p ' ________'
+p '1| - - - |'
+p '2| X - - |'
+p '3| - - - |'
+p ' ________'
+puts "\n\n"
+
+p "Camilo\'s turn:"
 # game.new_turn("Camilo", "A3")
+# game.show_board
+p '   A B C'
+p ' ________'
+p '1| - - - |'
+p '2| X - - |'
+p '3| 0 - - |'
+p ' ________'
+puts "\n\n"
+
+p "Carlos\' turn:"
+# game.new_turn("Carlos", "B2")
+# game.show_board
+p '   A B C'
+p ' ________'
+p '1| - - - |'
+p '2| X X - |'
+p '3| 0 - - |'
+p ' ________'
+puts "\n\n"
+
+p "Camilo\'s turn:"
+# game.new_turn("Camilo", "C4")
+# new_turn returns false on invalid move
+p 'Invalid move!' # until new_turn returns true
+p "Camilo\'s turn:"
+# game.new_turn("Camilo", "C3")
+# game.show_board
+p '   A B C'
+p ' ________'
+p '1| - - - |'
+p '2| X X - |'
+p '3| 0 - 0 |'
+p ' ________'
+puts "\n\n"
+
+p "Carlos\' turn:"
+# game.new_turn("Carlos", "B3")
+# game.show_board
+p '   A B C'
+p ' ________'
+p '1| - - - |'
+p '2| X X X |'
+p '3| 0 - 0 |'
+p ' ________'
+# if game.has_winner? returns true
+p "Player 1: #{player_1_name} won on turn: 5" # game.turns
+# else, game stopped because max # of turns reached
+# p 'Draw :('
+# p 'No winner on this round'
