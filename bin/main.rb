e0861fda77465de97610b4f934b71d518321e08e
ey-charles <carlos.castillo@hey.com>
2021-02-19 10:34:36 -0500

    Better logic for main.rb interface

--- bin/main.rb
+++ bin/main.rb
@@ -1,204 +1,94 @@
 #!/usr/bin/env ruby
 
+p '------------------------'
 p 'Welcome to Tic Tac Toe!'
-p 'Starting new game .  .  .'
+p '------------------------'
+p 'Starting new game . . .'
+
+# game = Game.new
 
 p 'What\'s the name of Player 1?'
 player_1_name = gets.chomp
-player_1_token = 'X'
+
+# game.add_player(player_1_name)
 
 p 'What\'s the name of Player 2?'
 player_2_name = gets.chomp
-player_2_token = 'O'
+
+# game.add_player(player_2_name)
+
 puts "\n\n"
 p '-----------------'
 p 'Start the game!'
 p '-----------------'
 p "Player 1: #{player_1_name}"
-p "Token: #{player_1_token}"
+p "Token: X"
 p '-----------------'
 p "Player 2: #{player_2_name}"
-p "Token: #{player_2_token}"
+p "Token: O"
 p '-----------------'
-puts "\n\n"
+puts "\n"
 
+p 'New empty board:'
 p '   A B C'
 p ' ________'
 p '1| - - - |'
 p '2| - - - |'
 p '3| - - - |'
 p ' ________'
-puts "\n\n"
 
-p "Carlos\' turn:"
+game_ends = false
 
+players = Array.new
+players.push(player_1_name)
+players.push(player_2_name)
+
+until game_ends
+  players.each { |player|
+
+    valid_turn = false
+
+    puts "\n"
+    p '-----------------'
+    p "Turn 2"
+    p "Player: #{player}"
+    p '-----------------'
+
+    until valid_turn
+      # game.show_board
+      puts 'Choose your move:'
+      puts "\n(Choose A2 to get out of loop)"
+
+      move = gets.chomp
+
+      valid_turn = true if move == "A2"
+
+      puts "\nInvalid move! Try again" unless valid_turn
+
+      puts "\n"
+    end
+
+    # game.check_for_win
+    # break if game_ends
+
+    game_ends = true
+  }
+end
+
+game_winner = true
+
+if game_winner
+  p "We have a winner!!! :D"
+else
+  p "Draw :("
+  p "Nobody won"
+end
+
+#  game.show_board
+p 'Final board:'
 p '   A B C'
 p ' ________'
-p '1| - - - |'
-p '2| X - - |'
-p '3| - - - |'
+p '1| X - - |'
+p '2| O X - |'
+p '3| O - X |'
 p ' ________'
-puts "\n\n"
-
-p "Camilo\'s turn:"
-
-p '   A B C'
-p ' ________'
-p '1| - - - |'
-p '2| X - - |'
-p '3| 0 - - |'
-p ' ________'
-puts "\n\n"
-
-p "Carlos\' turn:"
-
-p '   A B C'
-p ' ________'
-p '1| - - - |'
-p '2| X X - |'
-p '3| 0 - - |'
-p ' ________'
-puts "\n\n"
-
-p "Camilo\'s turn:"
-
-p 'Invalid move!'
-p "Camilo\'s turn:"
-
-p '   A B C'
-p ' ________'
-p '1| - - - |'
-p '2| X X - |'
-p '3| 0 - 0 |'
-p ' ________'
-puts "\n\n"
-
-p "Carlos\' turn:"
-
-p '   A B C'
-p ' ________'
-p '1| - - - |'
-p '2| X X X |'
-p '3| 0 - 0 |'
-p ' ________'
-
-p "Player 1: #{player_1_name} won on turn: 5"
-puts "\n"
-p 'Want to play again?'
-
-p '-----------------'
-p 'Start the game!'
-p '-----------------'
-p "Player 1: #{player_1_name}"
-p "Token: #{player_1_token}"
-p '-----------------'
-p "Player 2: #{player_2_name}"
-p "Token: #{player_2_token}"
-p '-----------------'
-puts "\n\n"
-
-p '   A B C'
-p ' ________'
-p '1| - - - |'
-p '2| - - - |'
-p '3| - - - |'
-p ' ________'
-puts "\n\n"
-
-p "Carlos\' turn:"
-
-p '   A B C'
-p ' ________'
-p '1| - - - |'
-p '2| X - - |'
-p '3| - - - |'
-p ' ________'
-puts "\n\n"
-
-p "Camilo\'s turn:"
-
-p '   A B C'
-p ' ________'
-p '1| - - - |'
-p '2| X - - |'
-p '3| 0 - - |'
-p ' ________'
-puts "\n\n"
-
-p "Carlos\' turn:"
-
-p '   A B C'
-p ' ________'
-p '1| - - - |'
-p '2| X X - |'
-p '3| 0 - - |'
-p ' ________'
-puts "\n\n"
-
-p "Camilo\'s turn:"
-
-p 'Invalid move!'
-p "Camilo\'s turn:"
-
-p '   A B C'
-p ' ________'
-p '1| - - - |'
-p '2| X X - |'
-p '3| 0 - 0 |'
-p ' ________'
-puts "\n\n"
-
-p "Carlos\' turn:"
-
-p '   A B C'
-p ' ________'
-p '1| - - X |'
-p '2| X X - |'
-p '3| 0 - 0 |'
-p ' ________'
-puts "\n\n"
-
-p "Camilo\'s turn:"
-
-p '   A B C'
-p ' ________'
-p '1| - - X |'
-p '2| X X 0 |'
-p '3| 0 - 0 |'
-p ' ________'
-puts "\n\n"
-
-p "Carlos\' turn:"
-
-p '   A B C'
-p ' ________'
-p '1| - X X |'
-p '2| X X 0 |'
-p '3| 0 - 0 |'
-p ' ________'
-puts "\n\n"
-
-p "Camilo\'s turn:"
-
-p '   A B C'
-p ' ________'
-p '1| 0 X X |'
-p '2| X X 0 |'
-p '3| 0 - 0 |'
-p ' ________'
-puts "\n\n"
-
-p "Carlos\' turn:"
-
-p '   A B C'
-p ' ________'
-p '1| 0 X X |'
-p '2| X 0 0 |'
-p '3| 0 X X |'
-p ' ________'
-puts "\n\n"
-
-p 'Draw :('
-p 'No winner this round.'
-puts "\n"
-p 'Want to play again?'
