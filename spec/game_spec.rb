7033a8f286c47e5ad25ee66ec5cf313a87c71273
Camilo Patino <jcamilop96@gmail.com>
2021-02-26 15:20:32 -0500

    Add tests for game spec

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -37,4 +37,70 @@ describe Game do
       expect(game.show_board).to include 'X'
     end
   end
+
+  describe '#new_turn' do
+    it 'returns false on an invalid turn' do
+      game.add_player(player1.name, player1.token)
+      game.add_player(player2.name, player2.token)
+      game.new_turn('Camilo', 'A2')
+      expect(game.new_turn('Carlos', 'A2')).to be false
+    end
+
+    it 'increases turn number after a valid move' do
+      game.add_player(player1.name, player1.token)
+      game.add_player(player2.name, player2.token)
+      expect { game.new_turn('Camilo', 'A2') }.to change { game.turns }.by(1)
+    end
+
+    it 'returns true on a valid turn' do
+      game.add_player(player1.name, player1.token)
+      game.add_player(player2.name, player2.token)
+      expect(game.new_turn('Camilo', 'A2')).to be true
+    end
+  end
+
+  context '#winner' do
+    it 'returns false when there is no winner' do
+      expect(game.winner).to be false
+    end
+
+    it 'returns true when there is a winner' do
+      game.add_player(player1.name, player1.token)
+      game.new_turn('Camilo', 'A1')
+      game.new_turn('Camilo', 'A2')
+      game.new_turn('Camilo', 'A3')
+      game.check_for_win
+      expect(game.winner).to be true
+    end
+  end
+
+  describe '#ends?' do
+    it 'returns true after maximum number of turns' do
+      game.add_player(player1.name, player1.token)
+      game.add_player(player2.name, player2.token)
+      game.new_turn('Camilo', 'A1')
+      game.new_turn('Carlos', 'A2')
+      game.new_turn('Camilo', 'A3')
+      game.new_turn('Camilo', 'B1')
+      game.new_turn('Carlos', 'B2')
+      game.new_turn('Camilo', 'B3')
+      game.new_turn('Carlos', 'C1')
+      game.new_turn('Camilo', 'C2')
+      game.new_turn('Carlos', 'C3')
+      expect(game.ends?).to be true
+    end
+
+    it 'returns true if there is a winner' do
+      game.add_player(player1.name, player1.token)
+      game.new_turn('Camilo', 'A1')
+      game.new_turn('Camilo', 'A2')
+      game.new_turn('Camilo', 'A3')
+      game.check_for_win
+      expect(game.ends?).to be true
+    end
+
+    it 'returns false when game continues' do
+      expect(game.ends?).to be false
+    end
+  end
 end
